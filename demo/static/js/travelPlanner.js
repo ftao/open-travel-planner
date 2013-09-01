a9013eb86fc475905f73d770fb9c5f3ca198976f
杨亚梦 <mz.yyam@gmail.com>
2013-09-01 15:28:02 +0800

    use json data for travelPlanner

---
+++ demo/static/js/travelPlanner.js
@@ -0,0 +1,70 @@
+function travelPlanner($scope){
+    $scope.plans = [
+        "方案一",
+        "方案二",
+        "方案三"
+    ];
+
+	$scope.selectedPlan = $scope.plans[0];
+
+	$scope.planClass = function(plan){
+		return plan === $scope.selectedPlan ? "list-group-item active" : "list-group-item";
+	}
+
+	$scope.changePlan = function(plan){
+		$scope.selectedPlan = plan;
+	}
+
+	$scope.planDetails = [
+		{
+			travelLine : ["上海","兰州","敦煌","张掖","西安","上海"],//行程线
+			travels : [//行程列表
+				{//行程
+					travelLine : ["上海","兰州"],
+					trafficWay : []
+				},
+				{
+					travelLine : ["兰州","敦煌"],
+					trafficWay : []
+				},
+				{
+					travelLine : ["敦煌","张掖"],
+					trafficWay : []
+				},
+				{
+					travelLine : ["张掖","西安"],
+					trafficWay : []
+				},
+				{
+					travelLine : ["西安","上海"],
+					trafficWay : []
+				}
+			]
+		},
+		{
+			travelLine : ["上海","兰州","张掖","敦煌","西安","上海"],//行程线
+			travels : [
+				{
+					travelLine : ["上海","兰州"],
+					trafficWay : []
+				},
+				{
+					travelLine : ["兰州","张掖"],
+					trafficWay : []
+				},
+				{
+					travelLine : ["张掖","敦煌"],
+					trafficWay : []
+				},
+				{
+					travelLine : ["敦煌","西安"],
+					trafficWay : []
+				},
+				{
+					travelLine : ["西安","上海"],
+					trafficWay : []
+				}
+			]
+		}
+	];
+}
